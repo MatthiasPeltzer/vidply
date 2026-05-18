69e5ecaceac76db71c3232e88b8fadc3fbd94a5e
Matthias Peltzer <7772293+MatthiasPeltzer@users.noreply.github.com>
2026-05-18 05:36:25 +0200

    release: v1.1.18

---
+++ dist/dev/vidply.HTML5Renderer-TZTNNAQK.js
@@ -0,0 +1,12 @@
+/*!
+ * VidPly v1.1.18 - Universal, Accessible Video Player
+ * (c) 2026 Matthias Peltzer
+ * Released under GPL-2.0-or-later License
+ */
+import {
+  HTML5Renderer
+} from "./vidply.chunk-ZFY7FNCU.js";
+export {
+  HTML5Renderer
+};
+//# sourceMappingURL=vidply.HTML5Renderer-TZTNNAQK.js.map
