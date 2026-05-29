4d74524c866999a43e054e2bbadf064d9c8c3d73
Matthias Peltzer <7772293+MatthiasPeltzer@users.noreply.github.com>
2026-05-29 05:03:38 +0200

    fix: update vendored streaming libs to hls.js 1.6.16 and dash.js 5.2.0

--- dist/legacy/vidply.js
+++ dist/legacy/vidply.js
@@ -1,5 +1,5 @@
 /*!
- * VidPly v1.1.18 - Universal, Accessible Video Player
+ * VidPly v1.1.19 - Universal, Accessible Video Player
  * (c) 2026 Matthias Peltzer
  * Released under GPL-2.0-or-later License
  */
